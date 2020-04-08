1238a144595db7093e822a94180d06a48f9406eb
etienne86 <barbier.et@gmail.com>
2020-04-08 14:39:48 +0200

    correct bad import

--- off_sub/management/commands/db_init.py
+++ off_sub/management/commands/db_init.py
@@ -9,7 +9,7 @@ import re
 import requests
 from django.core.management.base import BaseCommand
 
-from pur_beurre.off_sub.models import Category, Product, Store
+from off_sub.models import Category, Product, Store
 
 
 class Command(BaseCommand):
